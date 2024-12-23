d9e986152cc864857ea2ac17029d98c38721f9e4
Yukun Luo <yukunl@nvidia.com>
2024-12-23 17:25:21 +0800

    feat: start v02

---
+++ scenes/v02_building_a_math/func_utils.gd
@@ -0,0 +1,15 @@
+﻿class_name FuncUtils
+
+
+static func wave(x: float, t: float) -> float:
+	return sin(PI * (x + t))
+
+static func sin(x: float, t: float) -> float:
+	return 5 * sin((x + t))
+
+static func multi_wave(x: float, t: float) -> float:
+	return wave(x, t) + 0.5 * sin(2 * PI * (x + t))
+
+static func ripple(x: float, t: float) -> float:
+	var d = abs(x)
+	return sin(PI * (4 * d - t)) / (1 + 10 * d)
---
+++ scenes/v02_building_a_math/visual_function.gd
@@ -0,0 +1,35 @@
+﻿@tool
+extends Node3D
+
+class_name VisualFunction
+
+@export_enum("wave", "sin", "multi_wave", "ripple") var func_name: String
+@export var data_range: Vector3
+@export var shader: Shader
+
+var parsed: Callable
+
+func _ready() -> void:
+	for child in get_children():
+		remove_child(child)
+		child.free()
+	parsed = Callable(FuncUtils, func_name)
+
+	var x := data_range.x
+	var z: float = data_range.z
+	while x < data_range.y:
+		var node := CSGBox3D.new()
+		node.size = Vector3(z, z, z)
+		var material := ShaderMaterial.new()
+		material.shader = shader
+		node.material = material
+		node.position = Vector3(x, 0, 0)
+		add_child(node)
+		x += z
+
+
+func _process(_delta: float) -> void:
+	var time := Time.get_ticks_msec() / 1000.0
+	for child in get_children():
+		if child is CSGBox3D:
+			child.position.y = parsed.call(child.position.x, time)
