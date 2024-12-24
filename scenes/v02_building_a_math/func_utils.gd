46844c74deec1cc11e19b05d30d5496a48a10a90
Ukonn Ra <ukonnra@outlook.com>
2024-12-24 23:19:44 +0800

    feat: add commit lint

--- scenes/v02_building_a_math/func_utils.gd
+++ scenes/v02_building_a_math/func_utils.gd
@@ -1,18 +1,19 @@
 ﻿class_name FuncUtils
 
-
-static func wave(x: float, t: float) -> float:
-	return sin(PI * (x + t))
-
-
-static func sin(x: float, t: float) -> float:
-	return 5 * sin(x + t)
+static func wave(x: float, z: float, t: float) -> float:
+	return sin(PI * (x + z + t))
 
 
-static func multi_wave(x: float, t: float) -> float:
-	return wave(x, t) + 0.5 * sin(2 * PI * (x + t))
+static func sin(x: float, z: float, t: float) -> float:
+	return 5 * sin(x + z + t)
 
 
-static func ripple(x: float, t: float) -> float:
-	var d = abs(x)
+static func multi_wave(x: float, z: float, t: float) -> float:
+	var y1 = sin(PI * (x + 0.5 * t))
+	var y2 = 0.5 * sin(2 * PI * (z + t))
+	return 2.0 / 3.0 * (y1 + y2)
+
+
+static func ripple(x: float, z: float, t: float) -> float:
+	var d = sqrt(x ** 2 + z ** 2)
 	return sin(PI * (4 * d - t)) / (1 + 10 * d)
--- scenes/v02_building_a_math/visual_function.gd
+++ scenes/v02_building_a_math/visual_function.gd
@@ -16,20 +16,23 @@ func _ready() -> void:
 	parsed = Callable(FuncUtils, func_name)
 
 	var x := data_range.x
-	var z: float = data_range.z
 	while x < data_range.y:
-		var node := CSGBox3D.new()
-		node.size = Vector3(z, z, z)
-		var material := ShaderMaterial.new()
-		material.shader = shader
-		node.material = material
-		node.position = Vector3(x, 0, 0)
-		add_child(node)
-		x += z
+		var step: float =  data_range.z
+		var z           := data_range.x
+		while z < data_range.y:
+			var node     := CSGBox3D.new()
+			node.size = Vector3(step, step, step)
+			var material := ShaderMaterial.new()
+			material.shader = shader
+			node.material = material
+			node.position = Vector3(x, 0, z)
+			add_child(node)
+			z += step
+		x += step
 
 
 func _process(_delta: float) -> void:
 	var time := Time.get_ticks_msec() / 1000.0
 	for child in get_children():
 		if child is CSGBox3D:
-			child.position.y = parsed.call(child.position.x, time)
+			child.position.y = parsed.call(child.position.x, child.position.z, time)
